a07025833dd59e985075295730a523d020af413c
未来 <aiastia@users.noreply.github.com>
2017-09-19 16:46:41 +0800

    Update auth.py

--- shadowsocks/obfsplugin/auth.py
+++ shadowsocks/obfsplugin/auth.py
@@ -47,22 +47,16 @@ def create_auth_aes128_sha1(method):
     return auth_aes128_sha1(method, hashlib.sha1)
 
 obfs_map = {
-          'auth_sha1': (create_auth_sha1,),
-          'auth_sha1_compatible': (create_auth_sha1,),
-          'auth_sha1_v2': (create_auth_sha1_v2,),
-          'auth_sha1_v2_compatible': (create_auth_sha1_v2,),
-          'auth_sha1_v3': (create_auth_sha1_v3,),
-          'auth_sha1_v3_compatible': (create_auth_sha1_v3,),
           'auth_sha1_v4': (create_auth_sha1_v4,),
           'auth_sha1_v4_compatible': (create_auth_sha1_v4,),
-          'auth_aes128': (create_auth_aes128,),
           'auth_aes128_md5': (create_auth_aes128_md5,),
           'auth_aes128_md5_compatible': (create_auth_aes128_md5,),
           'auth_aes128_sha1': (create_auth_aes128_sha1,),
           'auth_aes128_sha1_compatible': (create_auth_aes128_sha1,),
-          'auth_chain_a': (create_auth_chain_a,),
-          'auth_chain_a_compatible': (create_auth_chain_a,),
+		  
+          
   }
+
 
 def match_begin(str1, str2):
     if len(str1) >= len(str2):
